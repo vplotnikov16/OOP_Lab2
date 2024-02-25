c3df20df39411295b43f47415bdfb24c177e8791
v-plotnikov <villagerfeed@yandex.ru>
2024-02-25 19:23:51 +0500

    Проверка работы всех конструкторов путём статического создания объектов разными способами. Проверка работы деструктора.

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ class Triangle
 {
 public:
     float a, b, c;
+
     Triangle()
     {
         printf("Triange()\n");
         a = b = c = 1;
     }
+
     Triangle(const float a, const float b, const float c)
     {
         printf("Triangle(const float a, const float b, const float c)\n");
@@ -17,13 +19,15 @@ public:
         this->b = b;
         this->c = c;
     }
-    Triangle(const Triangle &t)
+
+    Triangle(const Triangle& t)
     {
         printf("Triangle(const Triangle &t)\n");
         a = t.a;
         b = t.b;
         c = t.c;
     }
+
     ~Triangle()
     {
         printf("%f, %f, %f\n", a, b, c);
@@ -33,5 +37,10 @@ public:
 
 int main()
 {
+    {
+        Triangle t;
+        Triangle t2(3, 4, 5);
+        Triangle t3(t2);
+    }
     return 0;
 }
